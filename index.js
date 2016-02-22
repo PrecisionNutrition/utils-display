2286cce94098e5898e45ea366485887e95636ae3
Jeff Jewiss <jeffjewiss@gmail.com>
2016-02-22 13:30:50 -0500

    Updates package.json and adds breakpoint versions

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 require('./lib/display.css');
+require('./lib/display-dbl.css');
+require('./lib/display-dsk.css');
+require('./lib/display-lap.css');
+require('./lib/display-max.css');
+require('./lib/display-plm.css');
+require('./lib/display-prt.css');
